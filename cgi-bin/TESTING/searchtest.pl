42e99e10eeddd2743a6c5208f8c8fac967ea1e27
Joseph Southan <joseph@southanuk.co.uk>
2013-02-15 15:13:19 +0000

    Changed Search IDs for index, Changed Test script

--- cgi-bin/TESTING/searchtest.pl
+++ cgi-bin/TESTING/searchtest.pl
@@ -1,6 +1,7 @@
 #! /usr/bin/perl -w
 use strict;
 use SOAP::Lite;
+use Data::Dumper;
 my $soap = SOAP::Lite
 				#The package to call
 				->uri('ChromoDB')
@@ -11,4 +12,5 @@ my ($query, $type) = @ARGV;
 #print $query;
 #print $type;
 print my $returnSearch = $soap->getSearchResults($query,$type)->result;
-print my $hello = $soap->showCodingSeq($query)->result;
+print Dumper $returnSearch;
+#print my $hello = $soap->showCodingSeq($query)->result;
