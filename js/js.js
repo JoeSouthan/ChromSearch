223223f5a8b203aaa90778d52b4acc4a11a0c7b7
Joseph Southan <joseph@southanuk.co.uk>
2013-03-14 23:58:21 +0000

    JS: New AJAXified site. Favicon.

--- js/js.js
+++ js/js.js
@@ -12,37 +12,91 @@ function centerPopup(){
         "left" : winw/2-popw/2  
     }); 
 }
-$(document).ready(function(){ 
-  $("#show1").click(function() { 
-    $("#SequenceDNA").slideToggle("fast");
-  });
-  $("#show2").click(function() { 
-    $("#SequenceAA").slideToggle("fast");
-  });
-  $("#show3").click(function() { 
-    $("#codonusage").slideToggle("fast");
-  });
-  // $("#show4").click(function() { 
-    // $("#cutter").slideToggle("fast");
-  // });
-  $("#showhelp").click(function() {
-	  $("#overlay").fadeIn("fast");
-	  $("#help").slideDown("fast");
-	  centerPopup();
-	});
-  $("#closepopup").click(function() {
-	  $("#overlay").fadeOut("fast");
-	  $("#help").slideUp("fast");
-	});
-  $("#overlay").click(function() {
-	  $("#overlay").fadeOut("fast");
-	  $("#help").slideUp("fast");
-	});
-  $("#showadvanced").click(function() {
-    $("#advanced").slideToggle("fast");
-	});
-  $(window).resize(function() {  
-	centerPopup();  
-	}); 
-});
 
+function loadit(url) {
+	$("#main").load(url);
+}
+function closeHelp() {
+	  $("#overlay").fadeOut("fast");
+	  $("#help").slideUp("fast");
+}
+function openHelp() {
+				$("#overlay").fadeIn("fast");
+				$("#help").slideDown("fast");
+
+}
+$(document).ready(function(){ 
+	//Existing 
+	$("#show1").click(function() { 
+		$("#SequenceDNA").slideToggle("fast");
+	});
+	$("#show2").click(function() { 
+		$("#SequenceAA").slideToggle("fast");
+	});
+	$("#show3").click(function() { 
+		$("#codonusage").slideToggle("fast");
+	});
+	// $("#show4").click(function() { 
+		// $("#cutter").slideToggle("fast");
+	// });
+	$("#closepopup").click(function() {
+		closeHelp();
+	});
+	$("#overlay").click(function() {
+		closeHelp();
+	});
+	$("#showadvanced").click(function() {
+		$("#advanced").slideToggle("fast");
+	});
+	$(window).resize(function() {  
+		centerPopup();  
+	}); 
+//---------------------------------//
+	//clone the main
+	var cloned = $("#main").clone();
+	//Ajax
+	$.ajaxSetup({
+        beforeSend: function(xhr, status) {
+            $("#loader").fadeIn("fast");
+			$("#overlay").fadeIn("fast");
+        },
+		success: function(xhr, status) {
+			$("#loader").fadeOut("fast");
+			$("#overlay").fadeOut("fast");
+		},
+		error: function(jqXHR, exception) {
+			alert("Ajax error: " + jqXHR.status +".");
+		},
+		cache:true
+	});
+	//set up address bar
+	$.address.crawlable(true);
+	$.address.init(function(event) {
+		$('a').address(function() {
+			return $(this).attr('href').replace(location.pathname, '');
+
+		});
+	})
+	.change (function(event) {
+		//$("#main").load($('[rel=address:' + event.value+ ']').attr('href'));
+		//alert(""+event.value+"");
+		//loadit($(this).attr('href'));
+		switch (true) {
+			case(event.value == undefined):
+				alert("error");
+			case(event.value == "/"):
+				closeHelp();
+				$("#main").html(cloned);
+				break;
+			case(event.value == "/help"):
+				openHelp();
+				centerPopup();
+				break;
+			case(event.value == "/submit"):
+				closeHelp();
+				//alert($('#mainSearch').serialize());
+				$("#main").load("cgi-bin/search_results.pl?"+$('#mainSearch').serialize());
+				break;
+		}
+		});
+});
