8b6baf1d5fe98ba128ebe3b1fe7376414592478e
Joseph Southan <joseph@southanuk.co.uk>
2013-03-19 14:40:39 +0000

    Wrote JSON generator

--- cgi-bin/Modules/GenJSON.pm
+++ cgi-bin/Modules/GenJSON.pm
@@ -2,8 +2,73 @@
 package GenJSON;
 use strict;
 use JSON;
-use Chrom
+#use ChromoDB;
 use Exporter;
+use Data::Dumper;
 our @ISA = qw(Exporter);
 our @EXPORT = qw ();
 
+sub testJSONSearch {
+	my $json = JSON->new;
+	my @array = qw( test appples cake cats );
+	my %data = ( 
+		"gene1" => {
+				"accession" => "Test",
+				"length"=> 123,
+				"sequencefeatures" => [@array]
+			},
+		"gene2" => {
+				"accession" => "Test2",
+				"length"=> 1234,
+				"sequencefeatures" => [@array]
+			},
+		"gene3" => {
+				"accession" => "Test3",
+				"length"=> 12345,
+				"sequencefeatures" => [@array]
+			}
+	);
+	
+	#new element
+	$data{'gene4'}{'accession'} = "test4";
+	$data{'gene4'}{'length'} = 123456;
+	$data{'gene4'}{'sequencefeatures'} = [@array];
+	$data{'gene4'}{'somethingelse'} = "Something";
+	#Prints "Test2"
+	#print $data{'gene2'}{'accession'};
+	return $json->encode(\%data);
+}
+sub testJSONSingle {
+	my $json = JSON->new;
+	my @array = qw( test appples cake cats );
+	my %data = ( 
+		"gene1" => {
+				"accession" => "Test",
+				"length"=> 123,
+				"sequencefeatures" => [@array]
+			}
+	);
+	return $json->encode(\%data);
+}
+sub doSearch {
+	my $json = JSON->new;
+	my ($query,$type)= @_;
+	my %result = ChromoDB::getSearchResults($query,$type);
+	return $json->encode(\%result);
+
+}
+sub doSingle {
+	my $json = JSON->new;
+	my ($query)= @_;
+	my %result = ChromoDB::shwoSingle($query);
+	return $json->encode(\%result);
+
+}
+sub error {
+	my $json = JSON->new;
+	my %error = (
+		"error" => "There was an error"
+	);
+	return $json->encode(\%error);
+}
+1;
--- cgi-bin/Modules/json.pl
+++ cgi-bin/Modules/json.pl
@@ -1,5 +1,53 @@
 #! /usr/bin/perl -w
+#
+#	Script to serve JSON
+#
 use strict;
 use JSON;
 use GenJSON;
+use CGI;
+use CGI::Carp qw(warningsToBrowser fatalsToBrowser); 
 use Data::Dumper;#Debug
+my $cgi = new CGI;
+my $json = JSON->new;
+my @params= $cgi->param();
+
+my ($selector, $query,$type);
+# json.pl?selector=(search/single)&$query=(query)&type=(searchType)
+foreach my $params (@params) {
+	if ($params eq "query") {
+		$query = $cgi->param($params);
+	} elsif ($params eq "searchType") {
+		$type = $cgi->param($params);
+	} elsif ($params eq "selector") {
+		$selector = $cgi->param($params);
+	} 
+}
+
+
+#Debug
+# $query = "2780780";
+# $type = "GeneID";
+# $selector = "search";
+
+
+#Print the JSON header
+print $cgi->header('application/json');
+
+#Time for logic!
+unless (defined ($selector) or defined ($query) or defined ($selector) or defined ($type)) {
+	print GenJSON::error();
+} else {
+	#User wants a search
+	if ( $selector eq "search") {
+		print GenJSON::testJSONSearch();
+		#my $result = GenJSON::doSearch($query,$type);
+	#User Wants a single gene
+	} elsif ($selector eq "single") {
+		print GenJSON::testJSONSingle();
+	#Catch all
+	} else {
+		print GenJSON::error();
+	}
+
+}
